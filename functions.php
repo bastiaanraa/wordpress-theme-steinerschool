4bf9105f3a6e6a8bd4640904e2920c32f2f2f374
baraa <bastiaan.raa@ugent.be>
2016-06-21 22:14:39 +0200

    better modified date

--- functions.php
+++ functions.php
@@ -34,62 +34,37 @@ function register_my_menu() {
 }
 add_action( 'init', 'register_my_menu' );
 
-// Custom Functions to add SORT BY MODIFIED to post and page editor.
-// POSTS
-// Register the column
-function post_modified_column_register( $columns ) {
-    $columns['post_modified'] = __( 'Modified', 'mytextdomain' );
-    return $columns;
-}
-add_filter( 'manage_edit-post_columns', 'post_modified_column_register' );
 
-// Display the column content
-function post_modified_column_display( $column_name, $post_id ) {
-    if ( 'post_modified' != $column_name ){
-        return;
-    }
-    $post_modified = get_post_field('post_modified', $post_id);
-    if ( !$post_modified ){
-        $post_modified = '' . __( 'undefined', 'mytextdomain' ) . '';
-    }
-    echo $post_modified;
+add_action ( 'manage_pages_custom_column',	'rkv_heirch_columns',	10,	2	);
+add_filter ( 'manage_edit-page_columns',	'rkv_page_columns'				);
+function rkv_heirch_columns( $column, $post_id ) {
+	switch ( $column ) {
+	case 'modified':
+		$m_orig		= get_post_field( 'post_modified', $post_id, 'raw' );
+		$m_stamp	= strtotime( $m_orig );
+		$modified	= date('Y/m/d H:i', $m_stamp );
+	       	$modr_id	= get_post_meta( $post_id, '_edit_last', true );
+	       	$auth_id	= get_post_field( 'post_author', $post_id, 'raw' );
+	       	$user_id	= !empty( $modr_id ) ? $modr_id : $auth_id;
+	       	$user_info	= get_userdata( $user_id );
+	
+	       	echo '<p class="mod-date">';
+	       	echo '<em>'.$modified.'</em><br />';
+	       	echo 'by <strong>'.$user_info->display_name.'<strong>';
+	       	echo '</p>';
+		break;
+	// end all case breaks
+	}
 }
-add_action( 'manage_posts_custom_column', 'post_modified_column_display', 10, 2 );
-
-// Register the column as sortable
-function post_modified_column_register_sortable( $columns ) {
-    $columns['post_modified'] = 'post_modified';
-    return $columns;
+function rkv_page_columns( $columns ) {
+	$columns['modified']	= 'Last Modified';
+	return $columns;
 }
-add_filter( 'manage_edit-post_sortable_columns', 'post_modified_column_register_sortable' );
-//PAGES
-// Register the column
-function page_modified_column_register( $columns ) {
-    $columns['page_modified'] = __( 'Modified', 'mytextdomain' );
-    return $columns;
-}
-add_filter( 'manage_edit-page_columns', 'page_modified_column_register' );
-
-// Display the column content
-function page_modified_column_display( $column_name, $page_id ) {
-    if ( 'page_modified' != $column_name ){
-        return;
-    }
-    $page_modified = get_post_field('post_modified', $page_id);
-    if ( !$page_modified ){
-        $page_modified = '' . __( 'undefined', 'mytextdomain' ) . '';
-    }
-    echo $page_modified;
-}
-add_action( 'manage_pages_custom_column', 'page_modified_column_display', 10, 2 );
 
 // Register the column as sortable
-function page_modified_column_register_sortable( $columns ) {
-    $columns['page_modified'] = 'page_modified';
+function rkv_page_modified_column_register_sortable( $columns ) {
+    $columns['modified'] = 'modified';
     return $columns;
 }
-add_filter( 'manage_edit-page_sortable_columns', 'page_modified_column_register_sortable' );
-
-// end SORT BY MODIFIED
-
+add_filter( 'manage_edit-page_sortable_columns', 'rkv_page_modified_column_register_sortable' );
 ?>
