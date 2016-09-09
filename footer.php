a47ddeb0dc5b8164a3ea77d5a17872b42b66bc5d
baraa <bastiaan.raa@ugent.be>
2016-09-09 09:37:31 +0200

    optimized

--- footer.php
+++ footer.php
@@ -18,15 +18,15 @@
 			<a href="#" style="cursor:hand;font-size: 8pt;text-align: center;" >Laat het ons weten!</a><br />
 			Vrije Rudolf Steinerschool Gent - Kasteellaan 54 - 9000 Gent - 09/235.28.00 - 
 			<a style="font-size: 8pt;text-align: center;" href="mailto:info@steinerschoolgent.be?subject=Informatie over de school via www.steinerschoolgent.be">info@steinerschoolgent.be</a>
-			 - <a style="font-size: 8pt;text-align: center;" href="http://feeds.feedburner.com/VrijeRudolfSteinerschoolGent">RSS</a>
 		</div>
 	
 	</div>
 </footer>
 </div> <!-- wrap -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<link rel="stylesheet" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js">
+
 <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/steinerschool.js"></script>
 <?php wp_footer(); ?>
 </body></html>
--- header.php
+++ header.php
@@ -1,13 +1,13 @@
-<!DOCTYPE HTML>
-<html <?php language_attributes(); ?> >
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="nl"> <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
 	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<!--[if lt IE 9]>
-	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
-	<![endif]-->
 	<?php wp_head(); ?>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
@@ -16,9 +16,13 @@
 	<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/bootstrap-overrides.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/steinerschool.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 </head>
 
 <body>
+	<!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
 	<header class="container masthead" role="banner">
 		<div class='fixed sitetitle'>
 			<?php bloginfo( 'name' ); ?>
