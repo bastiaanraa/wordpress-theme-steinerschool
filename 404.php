97b6aefa298d0570b1cef57c0cb83b8b9505f111
baraa <bastiaan.raa@ugent.be>
2016-09-17 15:14:31 +0200

    added 404-template

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<div id="content"> 
+	<div class="row">
+		<h1>Pagina niet gevonden</h1>
+		<div>
+			De pagina die u zonet heeft opgevraagd, bestaat niet op dit webadres. Deze pagina werd mogelijk verplaatst, vervangen of verwijderd.
+		</div>
+	</div>
+</div><!--#content-->
+
+<?php get_footer(); ?>
